b6bb26f7790b98c929e8aca13bf058523a9e6f0f
Daniel Levi-Minzi <dleviminzi@gmail.com>
2023-08-18 13:42:02 -0400

    add minor versions and clarifying comment for models

--- anthropic_models.go
+++ anthropic_models.go
@@ -4,6 +4,12 @@ package anthrogo
 type AnthropicModel string
 
 const (
+	// ModelClaude2 is the latest major version of Claude 2.0 
 	ModelClaude2        AnthropicModel = "claude-2"
+
+	// ModelClaudeInstant1 is the latest major version of Claude Instant 1
 	ModelClaudeInstant1 AnthropicModel = "claude-instant-1"
+
+	ModelClaudeInstant1Dot1 AnthropicModel = "claude-instant-1.1"
+	ModelClaudeInstant1Dot2 AnthropicModel = "claude-instant-1.1"
 )
