65c1ca2ec4631b52f45466193a35144f7dcf5386
Daniel Levi-Minzi <dleviminzi@gmail.com>
2023-12-23 18:22:29 -0500

    update & fix model versions

--- anthropic_models.go
+++ anthropic_models.go
@@ -4,12 +4,10 @@ package anthrogo
 type AnthropicModel string
 
 const (
-	// ModelClaude2 is the latest major version of Claude 2.0 
-	ModelClaude2        AnthropicModel = "claude-2"
+	ModelClaude2     AnthropicModel = "claude-2"
+	ModelClaude2Dot1 AnthropicModel = "claude-2.1"
 
-	// ModelClaudeInstant1 is the latest major version of Claude Instant 1
-	ModelClaudeInstant1 AnthropicModel = "claude-instant-1"
-
+	ModelClaudeInstant1     AnthropicModel = "claude-instant-1"
 	ModelClaudeInstant1Dot1 AnthropicModel = "claude-instant-1.1"
-	ModelClaudeInstant1Dot2 AnthropicModel = "claude-instant-1.1"
+	ModelClaudeInstant1Dot2 AnthropicModel = "claude-instant-1.2"
 )
