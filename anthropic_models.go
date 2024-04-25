e8fb64e38585f1bcf9190ae7b65494e51fe75ec3
Daniel Levi-Minzi <dleviminzi@gmail.com>
2024-04-25 17:19:29 -0400

    add haiku and remove old models

--- anthropic_models.go
+++ anthropic_models.go
@@ -7,11 +7,10 @@ type AnthropicModel string
 const (
 	ModelClaude3Opus   AnthropicModel = "claude-3-opus-20240229"
 	ModelClaude3Sonnet AnthropicModel = "claude-3-sonnet-20240229"
+	ModelClaude3Haiku  AnthropicModel = "claude-3-haiku-20240307"
 
-	ModelClaude2     AnthropicModel = "claude-2"
+	ModelClaude2     AnthropicModel = "claude-2.0"
 	ModelClaude2Dot1 AnthropicModel = "claude-2.1"
 
-	ModelClaudeInstant1     AnthropicModel = "claude-instant-1"
-	ModelClaudeInstant1Dot1 AnthropicModel = "claude-instant-1.1"
 	ModelClaudeInstant1Dot2 AnthropicModel = "claude-instant-1.2"
 )
